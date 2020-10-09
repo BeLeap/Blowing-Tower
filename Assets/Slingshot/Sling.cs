137afbbad26e52ee99d891af01ddf1f0007a1237
BeLeap <changseo_jang@korea.ac.kr>
2020-10-10 00:28:09 +0900

    Grab Slingshot and Return

---
+++ Assets/Slingshot/Sling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Sling : MonoBehaviour
+{
+    private Hand hand;
+
+    private void OnAttachToHand(Hand attachedHand)
+    {
+        hand = attachedHand;
+    }
+
+    private void OnDetachFromhand(Hand hand)
+    {
+        Debug.Log("Detach!!");
+        Destroy(gameObject);
+    }
+}

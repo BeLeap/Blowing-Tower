7d099552b38a79c72f33d947d4386ad1501694eb
BeLeap <changseo_jang@korea.ac.kr>
2020-10-10 01:38:09 +0900

    Add Stone

--- Assets/Slingshot/Stone.cs
+++ Assets/Slingshot/Stone.cs
@@ -5,16 +5,42 @@ using Valve.VR.InteractionSystem;
 
 public class Stone : MonoBehaviour
 {
-    private Hand hand;
+    private Interactable interactable;
 
-    private void OnAttachToHand(Hand attachedHand)
+    private void Start()
     {
-        hand = attachedHand;
+        interactable = GetComponent<Interactable>();
     }
 
-    private void OnDetachFromhand(Hand hand)
+    private Vector3 originalPositon;
+    private Quaternion originalRotation;
+
+    private Hand.AttachmentFlags attachmentFlags =
+        Hand.defaultAttachmentFlags
+        & (~Hand.AttachmentFlags.SnapOnAttach)
+        & (~Hand.AttachmentFlags.DetachOthers)
+        & (~Hand.AttachmentFlags.VelocityMovement);
+
+    private void HandHoverUpdate(Hand hand)
     {
-        Debug.Log("Detach!!");
-        Destroy(gameObject);
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            originalPositon = transform.position;
+            originalRotation = transform.rotation;
+
+            hand.HoverLock(interactable);
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+        }
+        else if(isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+
+            transform.position = originalPositon;
+            transform.rotation = originalRotation;
+        }
     }
 }

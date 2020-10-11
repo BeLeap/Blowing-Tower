82a1397d4581afd0dc84b23dce4094587df89255
BeLeap <changseo_jang@korea.ac.kr>
2020-10-11 11:41:52 +0900

    Merge

---
+++ Assets/Wall & Destruction/Scripts/CollisionEvent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CollisionEvent : MonoBehaviour
+{
+    public float collisionMagnitude = 2;
+    public float collisionEventLimit = 3;
+    public SoundManager soundManager;
+    public GameObject particle;
+    
+
+    uint collisionEventCount;
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(particle);
+        soundManager.playSound();
+        Destroy(gameObject);
+    }
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionEventCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collisionEventCount);
+        if (collision.relativeVelocity.magnitude > collisionMagnitude){
+            Debug.Log("CollisionCount++");
+            collisionEventCount++;
+
+            if(collisionEventCount == collisionEventLimit)
+            {
+                Instantiate(particle);
+                soundManager.playSound();
+                Destroy(gameObject);
+                // StartCoroutine(Timer());
+            }   
+        }
+    }
+}
---
+++ Assets/Wall & Destruction/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void playSound()
+    {
+        audioSource.Play();
+    }
+}

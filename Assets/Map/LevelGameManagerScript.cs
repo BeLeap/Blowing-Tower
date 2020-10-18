10e625d35a47d4ab0203f23d0a6d831c130423f8
BeLeap <changseo_jang@korea.ac.kr>
2020-10-18 13:49:08 +0900

    Add LevelManager for level complete

---
+++ Assets/Map/LevelGameManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelGameManagerScript : MonoBehaviour
+{
+    GameObject[] enemies;
+    private GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        Debug.Log(enemies.Length);
+
+        if(enemies.Length == 0)
+        {
+            Destroy(Player);
+            SceneManager.LoadScene("LevelSelection", LoadSceneMode.Additive);
+        }
+    }
+}

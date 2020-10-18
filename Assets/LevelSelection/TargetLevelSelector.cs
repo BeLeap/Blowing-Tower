100b3e6616819a045a0258a391b4822e7ebda68a
BeLeap <changseo_jang@korea.ac.kr>
2020-10-18 11:07:13 +0900

    Rebase from 'level_selection'

---
+++ Assets/LevelSelection/TargetLevelSelector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TargetLevelSelector : MonoBehaviour
+{
+    public int level;
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene("level_" + level.ToString(), LoadSceneMode.Additive);
+    }
+}
